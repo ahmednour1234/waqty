c20ce432d0298c1a73ef000d226fa7d21bbfb675
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-19 05:29:08 +0200

    qweqw

--- lang/ar/validation.php
+++ lang/ar/validation.php
@@ -5,11 +5,16 @@ return [
     'email' => 'يجب أن يكون حقل :attribute عنوان بريد إلكتروني صالح.',
     'string' => 'يجب أن يكون حقل :attribute نصاً.',
     'max' => [
+        'numeric' => 'يجب ألا يزيد حقل :attribute عن :max.',
         'string' => 'يجب ألا يزيد حقل :attribute عن :max حرف.',
         'file' => 'يجب ألا يزيد حجم ملف :attribute عن :max كيلوبايت.',
+        'array' => 'يجب ألا يحتوي حقل :attribute على أكثر من :max عناصر.',
     ],
     'min' => [
+        'numeric' => 'يجب ألا يقل حقل :attribute عن :min.',
         'string' => 'يجب ألا يقل حقل :attribute عن :min حرف.',
+        'array' => 'يجب أن يحتوي حقل :attribute على :min عناصر على الأقل.',
+        'file' => 'يجب ألا يقل حجم ملف :attribute عن :min كيلوبايت.',
     ],
     'boolean' => 'يجب أن يكون حقل :attribute true أو false.',
     'integer' => 'يجب أن يكون حقل :attribute رقماً صحيحاً.',
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -5,11 +5,16 @@ return [
     'email' => 'The :attribute field must be a valid email address.',
     'string' => 'The :attribute field must be a string.',
     'max' => [
+        'numeric' => 'The :attribute field must not be greater than :max.',
         'string' => 'The :attribute field must not be greater than :max characters.',
         'file' => 'The :attribute field must not be greater than :max kilobytes.',
+        'array' => 'The :attribute field must not have more than :max items.',
     ],
     'min' => [
+        'numeric' => 'The :attribute field must be at least :min.',
         'string' => 'The :attribute field must be at least :min characters.',
+        'array' => 'The :attribute field must have at least :min items.',
+        'file' => 'The :attribute field must be at least :min kilobytes.',
     ],
     'boolean' => 'The :attribute field must be true or false.',
     'integer' => 'The :attribute field must be an integer.',
